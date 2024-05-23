93c67cc220631cef48f28edf9e912647629ec5fa
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-05-22 21:41:38 -0300

    criado menu e interação com usuario

--- src/ConversordeMoedas.java
+++ src/ConversordeMoedas.java
@@ -7,14 +7,13 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConversordeMoedas {
+
     public Moedas convereterMoedas(String moedadeEntrada, String moedaDeSaida, double valor) {
 
         HttpClient client = HttpClient.newHttpClient();
         String key = "3b34e66c8f52b4729765c003";
 
-        URI endereco = URI.create("https://v6.exchangerate-api.com/v6/" +
-                key +
-                "/pair/" +
+        URI endereco = URI.create("https://v6.exchangerate-api.com/v6/" + key + "/pair/" +
                 moedadeEntrada + "/" + moedaDeSaida + "/" + valor);
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,40 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         ConversordeMoedas conversordeMoedas = new ConversordeMoedas();
+        Scanner leitura = new Scanner(System.in).useLocale(Locale.US);
+        int opcao = 0;
+        double valor = 0;
 
-        Moedas valorConvertido = conversordeMoedas.convereterMoedas("USD", "BRL", 1);
+        String menu = """
+                ** Digite a sua opção **
+                1 - Converter de BRL para USD
+                2 - Converter de USD para BRL
+                3 - Sair
+                """;
 
-        System.out.println(valorConvertido);
+        while (opcao != 3) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+            if(opcao == 1) {
+                System.out.println("Digite valor para converter:");
+                valor = leitura.nextDouble();
+                Moedas valorConvertido = conversordeMoedas.convereterMoedas("BRL", "USD", valor);
+                System.out.println(valorConvertido);
+            } else if (opcao == 2) {
+                System.out.println("Digite valor para converter:");
+                valor = leitura.nextDouble();
+                Moedas valorConvertido = conversordeMoedas.convereterMoedas("USD", "BRL", valor);
+                System.out.println(valorConvertido);
+            } else if (opcao != 3) {
+                System.out.println("opção invalida");
+            }
+        }
+
+
     }
 }
